8d5f9e476846d4838d691014d92ecccb02ec7418
Sudip Maji <sudip.maji@traceable.ai>
2023-11-25 18:27:54 +0530

    add support for s3 as backup location

--- backup.sh
+++ backup.sh
@@ -15,13 +15,14 @@ if [ -z "$BACKUP_LOCATION" ]; then
   exit 1
 fi
 
-if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
-  echo "Environment variable GOOGLE_APPLICATION_CREDENTIALS is required."
-  exit 1
-fi
+if [[ "$BACKUP_LOCATION" =~ ^gs.* ]]; then
+  if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
+    echo "Environment variable GOOGLE_APPLICATION_CREDENTIALS is required."
+    exit 1
+  fi
 
-BOTO_CONFIG_PATH=${BOTO_CONFIG_PATH:-/root/.boto}
-cat <<EOF > $BOTO_CONFIG_PATH
+  BOTO_CONFIG_PATH=${BOTO_CONFIG_PATH:-/root/.boto}
+  cat <<EOF > $BOTO_CONFIG_PATH
 [Credentials]
 gs_service_key_file = $GOOGLE_APPLICATION_CREDENTIALS
 [Boto]
@@ -33,6 +34,8 @@ default_api_version = 2
 [OAuth2]
 EOF
 
+fi
+
 sleep 15
 timestamp=$(date +%Y-%m-%d-%H-%M-%S)
 month=$(date +%Y-%m)
@@ -41,6 +44,14 @@ cd /data/zookeeper
 
 java $JAVA_OPTIONS -jar /opt/guano/guano-0.1.0.jar -v -s "$ZOOKEEPER_URL" -o "/data/zookeeper/zookeeper${timestamp}" -d "/" || exit 2
 tar cfz "zookeeper${timestamp}.tar.gz" "zookeeper${timestamp}" || exit 2
-gsutil cp "/data/zookeeper/zookeeper${timestamp}.tar.gz" "$BACKUP_LOCATION/$month/zookeeper${timestamp}.tar.gz" || exit 2
+
+if [[ "$BACKUP_LOCATION" =~ ^gs.* ]]; then
+  gsutil cp "/data/zookeeper/zookeeper${timestamp}.tar.gz" "$BACKUP_LOCATION/$month/zookeeper${timestamp}.tar.gz" || exit 2
+elif [[ "$BACKUP_LOCATION" =~ ^s3.* ]]; then
+  aws s3 cp "/data/zookeeper/zookeeper${timestamp}.tar.gz" "$BACKUP_LOCATION/$month/zookeeper${timestamp}.tar.gz" || exit 2
+else
+  echo "Unsupported BACKUP_LOCATION URL. Please provide a valid AWS S3 or Google Storage URL"
+  exit 1
+fi
 
 rm -rf  "/data/zookeeper/zookeeper${timestamp}" "/data/zookeeper/zookeeper${timestamp}.tar.gz" || exit 0
